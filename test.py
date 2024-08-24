e36220b494e422a9417abd7b835114a51892ba90
Difki Mahriza <Difky04zz@gmail.com>
2024-08-24 17:29:49 +0700

    belajar python part2 (cara kerja program dan bytecode)

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("apa kabar my friend?")
+a=12
+print(a)
+
+# kita bisa mengcompile python ke yang namanya BYTECODE
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile (nama file)
+# bedanya apa yg di compile dan interpreted? bedanya ialah yang di compile akan lebih cepat dari pada interpreted.
+# mungkin karena ini masih sedikit program yang kita buat maka perbedaanya sedikit, tapi smkin banyak program yang kita buat maka akan terasa waktu perbedaan antara program yang compile dengan interpreted waktu hasilnya keluar, jadi terbukti bahwa compile akan lebih cepat dengan interpreted
+# maka dari itu krna python bahasa interpreted maka hasilnya akan lebih lambat daripada bahasa compiled tapi itu bisa ditanggulangi dengan cara mengcompile file nya terlebih dahulu, sperti yang diajarkan diataas
