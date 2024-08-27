54b13390fdf8d85704b6f8243a282c4876e75528
Difki Mahriza <Difky04zz@gmail.com>
2024-08-28 06:02:34 +0700

    belajar python part7 (operasi aritmetika)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,10 +1,60 @@
-#belajar python part6
-#mengambil input dari user
+#belajar python part7
+#operasi aritmetika
 
-data = input("masukan data : ")
-#dalam kurung biasanya adalah teks pertanyaan
-print("data = ", data, type(data))
-#jika kita ingin mengambil data int
+#pertama kita buat sbuah variabel
 
-data_int=int(input("masukan data: "))
-print("data = ", data_int,type(data_int))
+a=10
+b=3
+
+#operasi tambah +
+hasil = a+b
+print(a,"+",b,"=",hasil)
+
+#operasi pengurangan -
+hasil = a-b
+print(a,"-",b,"=",hasil)
+
+#operasi perkalian *
+hasil = a*b
+print(a,"*",b,"=",hasil)
+
+#operasi pembagian /
+hasil = a/b
+print(a,"/",b,"=",hasil)
+
+#operasi eksponen (pangkat) **
+hasil = a**b
+print(a,"**",b,"=",hasil)
+
+#BEBERAPA OPERATOR YG SERING DIPAKAI DI BAHASA LAIN
+
+#operasi modulus % (sisa pembagian)
+
+hasil=a%b
+print(a,'%',b,"=",hasil)
+
+#operasi floor division //
+# ini adalah hasil pembagian yg dibulatkan, krna 10/3 = 3,33333 dia akan dibulatkan hanya menjadi 3
+hasil= a//b
+print(a,"//",b,"=",hasil)
+
+#PRIORITAS OPERASI (OPERASI MANA YG AKAN DIDLUANKAN)
+
+'''
+1.() operasi didalam kurung akan didluankan
+2.eksponen **
+3.perkalian dan kawan kawan * / % //
+4.pertambahan dan pengurangn + -
+'''
+x=3
+y=2
+z=4
+
+hasil = x ** z * y + x / y - x % z // y
+print(x,'**',z,'*',y,'+',x,'/',y,'-',x,'%',z,'//',y,'=',hasil)
+
+hasil = x + y * z
+print(x,'+',y,'*',z)
+
+hasil = (x+y) * z
+print('(',x,'+',y,")",'*',z)
