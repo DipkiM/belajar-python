ca55af48e9efad10dba45e2e18fcf5b89d93586b
Difki Mahriza <Difky04zz@gmail.com>
2024-09-04 05:37:49 +0700

    belajar python part10 (operasi logika atau boolean)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,81 +1,79 @@
-#BELAJAR JAVASCRIPT PART9
-#OPERASI KOMPARASI
+#BELAJAR PYTHON PART10
+#OPERASI LOGIKA ATAU BOOLEAN
 
-#setiap hasil komparasi bernilai boolean (true or false)
-#operator operatornya antara lain: >,<,>=,<=,==,!=,is,is not
+#not, or , and, xor
 
-#cntoh
-a=2
-b=5
+#NOT
+print('======NOT======')
+a = True
+c = not a
 
-#lebih besar dari (>)
-print('========== lebih besar dari (>)')
-hasil = a>7
-print(a,">",7,"=",hasil)
-hasil = a>1
-print(a,">",1,"=",hasil)
-hasil = a>2
-print(a,">",2,"=",hasil)
+print('data a =',a)
+print('--------------- NOT')
+print('data c =',c)
+# knpa data c=false? krena dia not a, dan a = trus berarti not true = false
 
-#kurang dari (<)
-print('========== kurang dari (<)')
-hasil = b<4
-print(b,"<",4,"=",hasil)
-hasil = b<6
-print(b,"<",6,"=",hasil)
-hasil = b<6.1
-print(b,"<",6.1,"=",hasil)
-hasil = b<2
-print(b,"<",2,"=",hasil)
 
-#lebih besar sama dengan (>=)
-print('========== lebih besar sama dengan (>=)')
-hasil = a>=2
-print(a,">=",2,"=",hasil)
-hasil = a>=4
-print(a,">=",4,"=",hasil)
-hasil = a>=1
-print(a,">=",1,"=",hasil)
+#OR
+print('======OR======')
+#kalau or kita harus punya dua buah nilai, contoh disini kita akan tambahkan b, dimana dia akan menjadi false
 
-#kurang dari sama dengan (<=)
-print('========== kurang dari sama dengan (<=)')
-hasil = b<=4
-print(b,"<=",4,"=",hasil)
-hasil = b<6
-print(b,"<=",6,"=",hasil)
-hasil = b<=5
-print(b,"<=",5,"=",hasil)
+#kalau OR jika salah satu pilihan nya TRUE, maka hasilnya akan TRUE
+a = False
+b = False
+c = a or b
+print(a,"or",b,"=",c)
+a = True
+b = True
+c = a or b
+print(a,"or",b,"  =",c)
+a = True
+b = False
+c = a or b
+print(a,"or",b," =",c)
+a = True
+b = True
+c = a or b
+print(a,"or",b,"  =",c)
 
-#sama dengan sama dengan (==)
-print('========== sama dengan sama dengan (==)')
-#nah kalau bgini dia akan mengecek, misal a=5, lalu kita buat program a==5, maka jawabn yang akan keluar adalah TRUE, karena 5=5, misal a==3, jawabny akan FALSE, jadi sama dengan sama dengan ini adalah untuk mengecek suatu data
+#AND
+#jika salah satunya ada yang FALSE, maka akan hasilnya false, dan jika kduanya bernilai sama maka nilainya akan sama juga
+print("========AND=========")
+a = False
+b = False
+c = a and b
+print(a,"and",b,"=",c)
+a = True
+b = False
+c = a and b
+print(a,"and",b,"  =",c)
+a = True
+b = False
+c = a and b
+print(a,"and",b," =",c)
+a = True
+b = True
+c = a and b
+print(a,"and",b,"  =",c)
 
-hasil = a==2
-print(a,"==",2,"=",hasil)#jawaban ini nanti akan TRUE
-hasil = a==1
-print(a,"==",1,"=",hasil)#dan jawaban ini akan FALSE
+#XOR
+#dia akan TRUE jika hanya salah satu saja yang TRUE, sisanya false, jika dia ada dua TRUE maka hasilnya FALSE
+print("========XOR=========")
+a = False
+b = False
+c = a ^ b
+print(a,"xor",b,"=",c)
+a = True
+b = False
+c = a ^ b
+print(a,"xor",b,"  =",c)
+a = True
+b = False
+c = a ^ b
+print(a,"xor",b," =",c)
+a = True
+b = True
+c = a ^ b
+print(a,"xor",b,"  =",c)
 
-#tidak sama dengan (!=)
-#ini adalah kebalikan dari sama dengan sama dengan, ini akan mengecek, bila perbandingan nya sama maka jawaban yang dihasilkan ialah FALSE, dan jika perbandingan nya hasil berbeda maka jawabn akan menjadi TRUE
-print('========== tidak sama dengan (!=)')
-hasil = a!=2
-print(a,"!=",2,"=",hasil)#jawaban ini nanti akan TRUE
-hasil = a!=1
-print(a,"!=",1,"=",hasil)#dan jawaban ini akan FALSE
-
-# "is" sebagai komparasi objek
-print('========== object identity (is)')
-x=5 #ini adalah assignment membuat objek
-y=6
-print('nilai x =',x,'id=',hex(id(x)))
-print('nilai x =',y,'id=',hex(id(y)))
-hasil = x is y
-print(x,"is",y,"=",hasil)#nah ini hasilnya akan TRUE, krena nilai x dan y sama, dan juga dia bukan literal
-hasil = x is 5
-print(x,"is",5,"=",hasil)#nah kalau ini akan menjadi peringatan, krna nilai x dan y walaupun sama tapi dia itu sebuah literal makanya nnti akan ada peringatan dari python ny
-
-# "is not" ini mirip dengan tidak sama dengan, jadi yang beda akan bernilai TRUE
-hasil = x is not y
-print("nilai x =",x,"id=",hex(id(x)))
-print("nilai x =",y,"id=",hex(id(y)))
-print (x,"is not",y,"=",hasil)
+#dan aturan ini akan berlaku dngn 3 variabel, dngn syarat dan aturan yang sama
