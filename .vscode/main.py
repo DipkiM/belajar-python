d47f3e0204bdfa444b18b05cb1e4e0ab028b0a9b
Difki Mahriza <Difky04zz@gmail.com>
2024-09-04 20:41:06 +0700

    belajar python part11 (latihan komparasi dan logika)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,79 +1,84 @@
-#BELAJAR PYTHON PART10
-#OPERASI LOGIKA ATAU BOOLEAN
+#BELAJAR PYTHON PART11
+#LATIHAN KOMPARASI DAN LOGIKA
 
-#not, or , and, xor
+#membuat gabungan area rentang dari angka
 
-#NOT
-print('======NOT======')
-a = True
-c = not a
+# ++++++++3---------10++++++++
 
-print('data a =',a)
-print('--------------- NOT')
-print('data c =',c)
-# knpa data c=false? krena dia not a, dan a = trus berarti not true = false
+inputuser = float(input("masukan angka yang bernilai kurang dari 3\n atau\n lebih besar dari 10\n"))
 
+#++++++++3---------
+#kurang dari
 
-#OR
-print('======OR======')
-#kalau or kita harus punya dua buah nilai, contoh disini kita akan tambahkan b, dimana dia akan menjadi false
+iskurangdari = (inputuser <3)
+print("kurang dari 3=", iskurangdari)
 
-#kalau OR jika salah satu pilihan nya TRUE, maka hasilnya akan TRUE
-a = False
-b = False
-c = a or b
-print(a,"or",b,"=",c)
-a = True
-b = True
-c = a or b
-print(a,"or",b,"  =",c)
-a = True
-b = False
-c = a or b
-print(a,"or",b," =",c)
-a = True
-b = True
-c = a or b
-print(a,"or",b,"  =",c)
+#-----------10+++++++++
+#lebih besar dari
 
-#AND
-#jika salah satunya ada yang FALSE, maka akan hasilnya false, dan jika kduanya bernilai sama maka nilainya akan sama juga
-print("========AND=========")
-a = False
-b = False
-c = a and b
-print(a,"and",b,"=",c)
-a = True
-b = False
-c = a and b
-print(a,"and",b,"  =",c)
-a = True
-b = False
-c = a and b
-print(a,"and",b," =",c)
-a = True
-b = True
-c = a and b
-print(a,"and",b,"  =",c)
+islebihbesar = (inputuser >10)
+print("lebih besar dari 10=", islebihbesar)
 
-#XOR
-#dia akan TRUE jika hanya salah satu saja yang TRUE, sisanya false, jika dia ada dua TRUE maka hasilnya FALSE
-print("========XOR=========")
-a = False
-b = False
-c = a ^ b
-print(a,"xor",b,"=",c)
-a = True
-b = False
-c = a ^ b
-print(a,"xor",b,"  =",c)
-a = True
-b = False
-c = a ^ b
-print(a,"xor",b," =",c)
-a = True
-b = True
-c = a ^ b
-print(a,"xor",b,"  =",c)
+#jikalau ingin menggabungkan dua buah pers (lebih dari, kurang dari dll) itu kita gunakan or
 
-#dan aturan ini akan berlaku dngn 3 variabel, dngn syarat dan aturan yang sama
+iscorrect = iskurangdari or islebihbesar
+print("angka yang anda masukan bernilai =", iscorrect)
+
+# -----3++++++++10------
+#ini adalah kasus irisan
+
+inputuser=float(input("masukan angka yang bernilai lebih dari 3\n dan\n kurang dari 10\n"))
+
+# ------3+++++++++++
+#lebih dari 3
+islebihbesar = inputuser >3
+print("lebih besar = ",islebihbesar)
+
+# +++++++++++10--------
+#kurang dari 10
+iskurangdari = inputuser <10
+print("kurang dari 10 =", iskurangdari)
+
+inputuser=float(input("masukan angka lebih besar dari 0\n dan\n kurang dari 5\n dan lebih besar dari 8\n dan kurang dari 11\n"))
+
+#--------0+++++++
+#lebih dari 0
+islebihbesar = inputuser > 0
+print("lebih besar dari 0 =", islebihbesar)
+
+#++++++++5--------
+#kurang dari 5
+iskurangdari = inputuser < 5
+print("kurang dari 5 =", iskurangdari)
+
+#---------8+++++++
+#lebih besar dari 8
+islebihbesar = inputuser > 8
+print("lebih besar dari 8 =", islebihbesar)
+
+#++++++++11-------
+#kurang dari 11
+iskurangdari = inputuser < 11
+print("kurang dari 11 =", iskurangdari)
+
+inputuser=float(input("masukan angka kurang dari 0\n dan\n lebih besar dari 5\n dan kurang dari 8\n dan lebih besar dari 11\n"))
+
+#+++++++0---------
+#kurang dari 0
+iskurangdari = inputuser < 0
+print("lebih besar dari 0 =", iskurangdari)
+
+#-------5+++++++
+#lebih dari dari 5
+islebihbesar = inputuser > 5
+print("kurang dari 5 =", islebihbesar)
+
+#+++++++8---------
+#kurang dari 8
+iskurangdari = inputuser < 8
+print("lebih besar dari 8 =", iskurangdari)
+
+#---------11++++++++
+#lebih dari 11
+islebihbesar = inputuser > 11
+print("kurang dari 11 =", islebihbesar)
