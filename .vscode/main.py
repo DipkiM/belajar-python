7f267455f33155db6c40b0ac8c1d44f9b22eb1c1
Difki Mahriza <Difky04zz@gmail.com>
2024-09-09 07:29:55 +0700

    belajar python part12 (operator bitwise)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,84 +1,61 @@
-#BELAJAR PYTHON PART11
-#LATIHAN KOMPARASI DAN LOGIKA
+#BELAJAR PYTHON PART12
+#operator bitwise
 
-#membuat gabungan area rentang dari angka
+a = 9
+b = 5
 
-# ++++++++3---------10++++++++
+#bitwise or (|)
+c = a|b
+print("\n ==========or=========")
+print("nilai :",a,', binary :', format(a,"08b"))
+#untuk format nnti di pelajari di string, ini cuman agar contoh aj dia diubah ke binary
+print("\n ===============|")
+print("nilai :",b,', binary :', format(b,"08b"))
+print("nilai :",c,', binary :', format(c,"08b"))
 
-inputuser = float(input("masukan angka yang bernilai kurang dari 3\n atau\n lebih besar dari 10\n"))
+#bitwise and (&)
+c = a & b
+print("\n ==========and=========")
+print("nilai :",a,', binary :', format(a,"08b"))
+print("\n ===============&")
+print("nilai :",b,', binary :', format(b,"08b"))
+print("nilai :",c,', binary :', format(c,"08b"))
 
-#++++++++3---------
-#kurang dari
 
-iskurangdari = (inputuser <3)
-print("kurang dari 3=", iskurangdari)
+#bitwise XOR (^)
+c = a ^ b
+print("\n ==========XOR=========")
+print("nilai :",a,', binary :', format(a,"08b"))
+print("\n ===============^")
+print("nilai :",b,', binary :', format(b,"08b"))
+print("nilai :",c,', binary :', format(c,"08b"))
 
-#-----------10+++++++++
-#lebih besar dari
+#bitwise not (~)
+c = ~a
+#ini adalah operator yang membalikan, jadi misal si a nilainya 9, maka nannti di c akan menjadi -10 dia akan dibalikan dan plus satu
+print("\n ==========not=========")
+print("nilai :",a,', binary :', format(a,"08b"))
+print("\n ===============(~)")
+print("nilai :",c,', binary :', format(c,"08b"))
 
-islebihbesar = (inputuser >10)
-print("lebih besar dari 10=", islebihbesar)
+#shifting (pergeseran)
 
-#jikalau ingin menggabungkan dua buah pers (lebih dari, kurang dari dll) itu kita gunakan or
+#shift right (>>)
+c=a>>2
+#iini berarti kita membuat a bergeser 2 kali ke kanan
+print("\n ==========>>=========")
+print("nilai :",a,', binary :', format(a,"08b"))
+print("\n ===============(>>)")
+print("nilai :",c,', binary :', format(c,"08b"))
 
-iscorrect = iskurangdari or islebihbesar
-print("angka yang anda masukan bernilai =", iscorrect)
+#shift left (<<)
+c=a<<2
+#iini berarti kita membuat a bergeser 2 kali ke kiri
+print("\n =========<<=========")
+print("nilai :",a,', binary :', format(a,"08b"))
+print("\n ===============<<)")
+print("nilai :",c,', binary :', format(c,"08b"))
 
-# -----3++++++++10------
-#ini adalah kasus irisan
 
-inputuser=float(input("masukan angka yang bernilai lebih dari 3\n dan\n kurang dari 10\n"))
 
-# ------3+++++++++++
-#lebih dari 3
-islebihbesar = inputuser >3
-print("lebih besar = ",islebihbesar)
 
-# +++++++++++10--------
-#kurang dari 10
-iskurangdari = inputuser <10
-print("kurang dari 10 =", iskurangdari)
-
-inputuser=float(input("masukan angka lebih besar dari 0\n dan\n kurang dari 5\n dan lebih besar dari 8\n dan kurang dari 11\n"))
-
-#--------0+++++++
-#lebih dari 0
-islebihbesar = inputuser > 0
-print("lebih besar dari 0 =", islebihbesar)
-
-#++++++++5--------
-#kurang dari 5
-iskurangdari = inputuser < 5
-print("kurang dari 5 =", iskurangdari)
-
-#---------8+++++++
-#lebih besar dari 8
-islebihbesar = inputuser > 8
-print("lebih besar dari 8 =", islebihbesar)
-
-#++++++++11-------
-#kurang dari 11
-iskurangdari = inputuser < 11
-print("kurang dari 11 =", iskurangdari)
-
-inputuser=float(input("masukan angka kurang dari 0\n dan\n lebih besar dari 5\n dan kurang dari 8\n dan lebih besar dari 11\n"))
-
-#+++++++0---------
-#kurang dari 0
-iskurangdari = inputuser < 0
-print("lebih besar dari 0 =", iskurangdari)
-
-#-------5+++++++
-#lebih dari dari 5
-islebihbesar = inputuser > 5
-print("kurang dari 5 =", islebihbesar)
-
-#+++++++8---------
-#kurang dari 8
-iskurangdari = inputuser < 8
-print("lebih besar dari 8 =", iskurangdari)
-
-#---------11++++++++
-#lebih dari 11
-islebihbesar = inputuser > 11
-print("kurang dari 11 =", islebihbesar)
