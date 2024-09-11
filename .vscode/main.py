174de2632648b0dbe03aa4355024d09a2b09c43b
Difki Mahriza <Difky04zz@gmail.com>
2024-09-12 06:08:49 +0700

    belajar python part14 (pengenalan string)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,98 +1,73 @@
 #BELAJAR PYTHON PART13
-#operator assignment (operasi yang dapat dilakukan dngan penyingkatan)
-#operasi ditambah dngn assignment
+#pengenalan string
 
-a = 5 #ini adalah contoh assignment
-print("nilai a =", a)
+# 1. cara membuat string
+#contoh kita punya variabel data
+data = "ini adalah string"#string ialah kumpulan dari karakter
+print(data, type(data))#ini adalah cara pemanggilan nya
 
-#contoh assignment yang disingkat
-# a =a +1
-# print('nilai a=', a)
+#cara membuat string
 
-#ini namanya OPERATOR TAMBAH
-#nah contoh yang telah disingkat
-a +=1 #ini adalah a = a+1
-print('nilai a+=1, nilai a mnjadi', a) # tuh hasilnya akan sama sperti yang diatas (tetap berhasil 6)
+'''
+    1. dengan cara menggunakan single quote ('.....')
+    2. dengan menggunakan double quote (".....")
+'''
+data = 'menggunakan single quote'
+print(data, type(data))
 
-#ini contoh OPERATOR KURANG
-a -=2 #ini adalah a= a-2
-print('nilai a-=2, nilai a mnjadi', a)
-#kenapa hasilnya 4 bukan 3? krena diatas a sudah berubah jadi 6
+data = "menggunakan double quote"
+print(data, type(data))
 
-#ini contoh OPERATOR PERKALIAN
-a *=5 #ini adalah a= a*5
-print('nilai a*=5, nilai a mnjadi', a)
+#kita juga bisa menggabungkan dua tadi
+print("'halo apa kabar?'")
+print('"halo apa kabar?"')
+print("ini adalah hari jum'at")
 
-#ini contoh OPERATOR PEMBAGIAN
-a /=2 #ini adalah a= a/2
-print('nilai a/=2, nilai a mnjadi', a)
+    # 2. menggunakan tanda \
+#BISA MEMBUAT (') MENJADI STRING
+#print('mari kita sholat jum'at') nah ini akan error karena dikira kode telah ditutup di bagian(jum)
+# maka dari itu kita bisa menggunakan tanda \ sperti ini:
+print('mari kita sholat jum\'at')
 
-#contoh baru
-b=10
-print('\n nilai b=',b)
+#BISA MENJADI BACKSLASH
+#print("c:\user\ucup") nah ini akan eror karena dia akan bingung backslash gk bisa dibaca, gimana cara agar backslash dapat dibaca sbgai string backslash? dngn cara menambahkan backslash lagi (\\)
+print("c:\\user\\ucup")
 
-#INI CONTOH OPERASI MODULUS dan FLOOR DIVISION
-b %=3
-print('nilai b%=3, nilai b menjadi', b)
+#BISA MENJADI TAB (\t)
+print("ucup\t otong, jauhan")#nah ini akan membuat space setelah ucup akan menjadi jarak ketika kita memakai tab
+print("ucup\t\t\t\t otong, semakin jauh")#kita bisa menambahkan (\t) terus menerus dan akan membuat space nya smakin lebar atau smakin jauh
 
-b=10
-print('\n nilai b=',b)
+#BISA MENJADI BACKSPACE (\b)
+print("ucup otong, ini jarak normal")#nah ini kan jarak normal, gimana kalau kita tambahkan (\b)
+print("ucup \botong, jadi deketan")#jarak dari ucup ke otong akan menjadi lebih dekat
 
-#INI CONTOH OPERASI LEFT DIVISION
-b //=3 #ini nanti hasilnya adalah 3, krena 10/3 dibulatkan mnjadi 3
-print('nilai b//=3, nilai b menjadi', b)
+#BISA MENJADI NEWLINE (\n) (BARIS BARU)
+print("baris pertama.\n baris kedua.")#ini yg kita kenal sbagai LF (line feed), ini biasany di mac os atau di linux dipakai nya LF (lihat kebawah kanan di samping {}python, nah ini biasanya newline menggunakan (\n))
+print("baris pertama.\r baris kedua.")#ini kita kenal sbagai CR (carriage return), ini biasanya dipakai sama yg dulu sperti commodore, acorn dll
+print("baris pertama.\r\n baris kedua.")#ini kita kenal sbagai CRLF (line feed carriage return), ini dipakai oleh windows, makanya terkadang suka beda ketika kita mau mendeteksi enter pada program windows dngn macos atau linux beda, krna yg windows pake CRLF sdngkan mereka menggunakan LF
 
-#CONTOH BARU
-#INI ADALAH PERPANGKATAN atau EKSPONEN
-a=5
-print('nilai a=,',a)
+    # 3. STRING LITERAL ATAU RAW (r".......")
+#misalkan kalian suka bikin kata (hati-hati)
+print("c:\new folder")#nah ini akan eror dong n nya akan menjadi LF. gimana caranya kita bikin dia tidak begitu?
+print("c:\\new folder")#ini bisa, cuman bgaimana kalau kita punya banyak yg begituan? nah ini bisa dipermudah dngn memakai RAW STRING
 
-a **=3
-print('nilai a**3=, nilai a mnjadi', a)
+#menggunakan RAW STRING
+print(r'c:\new folder')#nah ini akan dianggap sbagai raw string (semuanya akan di print apa yg kita taruh disitu )
 
-#OPERASI BITWISE
-#OR
-c=True
-print('nilai c=',c)
-c |= False
-print('nilai c|= false, maka nilai c =', c)# ini akan menjadi true, krena true dngn false akan mnjdi true
+#multiline untuk LITERAL STRING (sama sperti membuat command)(""".....""")
+print("""
+    Nama : Ucup
+    Kelas : 4SD
+    Umur : 10 tahun
+      """)
+#maka nanti akan di print sesuai yang kita tulis disini, mau enter nya dan sbagainya
 
-#contoh baru
-c=False
-c|=False
-print('nilai c|=false, nilai c=',c)#ini akan menjadi false, karena false dengan false hasilny false
-
-#AND
-c=True
-print('nilai c=',c)
-c &= False
-print('nilai c&= false, maka nilai c =', c)
-
-#contoh baru
-c=True
-c&=True
-print('nilai c&=true, nilai c=',c)
-
-#XOR
-c=True
-print('nilai c=',c)
-c ^= False
-print('nilai c^= false, maka nilai c =', c)
-
-#contoh baru
-c=True
-c^=True
-print('nilai c^=true, nilai c=',c)
-
-#geser geser
-d = 0b0100
-print('nilai d =',d)
-print('nilai d=', format(d,'04b'))
-
-d>>=2
-#ini akan menggeser angka  nya kekanan sebanyak 2 kali
-print('nilai d>>=2, maka nilai d menjadi', format(d, '04b'))
-
-d<<=2
-#ini akan menggeser angka nya kekiri sebanyak 1 kali
-print('nilai d<<=2, maka nilai d menjadi', format(d, '04b'))
+#kita juga bisa menggabungkan MULTILINE LITERAL STRING DAN RAW
+print(r"""
+    Nama : Ucup
+    Kelas : 4SD
+    Umur : 10 tahun
+    Zodiac : Piesces
+    Agama : islam
+    website : www.ucup.com\newID
+      """)
