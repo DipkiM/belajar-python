6614f8528eeea5791c688cd824871e75f3f91984
Difki Mahriza <Difky04zz@gmail.com>
2024-09-25 06:04:37 +0700

    belajar python part24 (while loop (perulangan))

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,47 +1,19 @@
-#belajar python part22
-#FOR LOOP (perulangan)
+#belajar python part24
+#while loop (perulangan)
 
-#contohny kita membuat perulangan angka
-# angka = 1
-# print(angka)
-# angka = angka + 1
-# print(angka)
-# angka = angka + 1#nah berarti jadi 3 kan
-# print(angka)
-# #tapi ini tidak efektif karena smkin bnyak perulangan line smkin bnyak dan panjang
+#while kondisi: kondisi disini itu menggunakan boolean (true or false)
+    #aksi a
+    #aksi b
+#akhir program
 
-#paling gampangnya ialah kita punya (FOR) lalu kita ada (KONDISI) dan juga (AKSI)
-# for konidisi:#jadi misalkan kondisi nya apa akan mlakukan aksi apa gitu
-#     aksi
+i = 10
+print(f"nilai i sekarang --> {i}")
 
-# for i in range (5):#nah ini artinya untuk semua i didalam range 5, jadi istilahnya smua angka di range 5 ini akan dikasi nama i
+while i > 5:
+    i -=1# nah dngn ini dia akan berhenti ktika angka ny sudah lebih kecil 5
+    print(f"nilai i sekarang --> {i}")
+    print("saya ganteng banget")
 
-# contoh
+print("iya tauuu kokkk :(")#nah disini dia akan mngulang trus menerus, knapa? karena i =10 dan kondisi nya memenuhi (True) jadi dia akan ngulang trus disitu (di print trus)
 
-#INI DENGAN LIST
-angka2_list = [1,2,3,4,5] #ini adalah angka yg berbentuk list
-
-for i in angka2_list:#jadi nanti setiap brulang si i itu akan jadi 1, jadi 2, 3 dst
-    print(f"angka i skrang --> {i}")
-print("akhir program 1\n")
-
-#INI DENGAN RANGE
-angka2_range = range(5)
-
-for i in angka2_range:
-    print(f"angka i skrang --> {i}")
-print("akhir program 2\n")
-
-angka2_range = range(1,5)#ini artinya dia mulai dari 1 dan diakhiri sblum 5 (atau dia cuman ngeprint sampai 4)
-
-for i in angka2_range:
-    print(f"angka i sekarang ==> {i}")
-print("akhir dari program 3\n")
-
-#MENGGUNAKAN STRING
-data_str = "saya ganteng abiesss"
-
-for huruf in data_str:
-    print(huruf)
-print("akhir dari program 4\n")# jadi dia akan ngeprint per huruf nya sampai selesai secara brulang
 
