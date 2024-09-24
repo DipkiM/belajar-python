d31f70394279841f7d4e289e70da3273ab3929cb
Difki Mahriza <Difky04zz@gmail.com>
2024-09-25 05:44:52 +0700

    belajar python part22 (for loop (perulangan))

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,32 +1,47 @@
-#belajar python part21
-#membuat kalkulator sederhana (LATIHAN)
+#belajar python part22
+#FOR LOOP (perulangan)
 
-#pertama kita ambil data user, yaitu angka pertama, angka kedua dan operasinya
-#kedua kita ambil cabang nya, misal dia milih operasi satu trus ya dia akan menjalankan operasi satu
+#contohny kita membuat perulangan angka
+# angka = 1
+# print(angka)
+# angka = angka + 1
+# print(angka)
+# angka = angka + 1#nah berarti jadi 3 kan
+# print(angka)
+# #tapi ini tidak efektif karena smkin bnyak perulangan line smkin bnyak dan panjang
 
-print(20*"=")
-print("MEMBUAT KALKULATOR SEDERHANA")
-print(20*"="+"\n")
+#paling gampangnya ialah kita punya (FOR) lalu kita ada (KONDISI) dan juga (AKSI)
+# for konidisi:#jadi misalkan kondisi nya apa akan mlakukan aksi apa gitu
+#     aksi
 
-angka1 = float(input("masukan angka pertama anda\n"))#knpa kita pakai float? karena ini bisa membuat kita menggunakan angka yang memiliki koma koma an, kalau int nnti dia error
-angka2 = float(input("masukan angka kedua anda\n"))
-operasi = input("masukan operasi anda (+,-,*,/,**)\n")
+# for i in range (5):#nah ini artinya untuk semua i didalam range 5, jadi istilahnya smua angka di range 5 ini akan dikasi nama i
 
-#PERCABANGAN NYA
-if operasi == "+":
-    hasil=(angka1 + angka2)
-    print(f"nilai dari perhitungan anda ialah = {hasil}")
-elif operasi == "-":
-    hasil=(angka1 - angka2)
-    print(f"nilai dari perhitungan anda ialah = {hasil}")
-elif operasi == "*":
-    hasil = (angka1 * angka2)
-    print(f"nilai dari perhitungan anda ialah = {hasil}")
-elif operasi == "/":
-    hasil = (angka1 / angka2)
-    print(f"nilai dari perhitungan anda ialah = {hasil}")
-elif operasi == "**":
-    hasil = (angka1 ** angka2)
-    print(f"nilai dari perhitungan anda ialah = {hasil}")
-else:
-    print("masukan operasi yang jelas")
+# contoh
+
+#INI DENGAN LIST
+angka2_list = [1,2,3,4,5] #ini adalah angka yg berbentuk list
+
+for i in angka2_list:#jadi nanti setiap brulang si i itu akan jadi 1, jadi 2, 3 dst
+    print(f"angka i skrang --> {i}")
+print("akhir program 1\n")
+
+#INI DENGAN RANGE
+angka2_range = range(5)
+
+for i in angka2_range:
+    print(f"angka i skrang --> {i}")
+print("akhir program 2\n")
+
+angka2_range = range(1,5)#ini artinya dia mulai dari 1 dan diakhiri sblum 5 (atau dia cuman ngeprint sampai 4)
+
+for i in angka2_range:
+    print(f"angka i sekarang ==> {i}")
+print("akhir dari program 3\n")
+
+#MENGGUNAKAN STRING
+data_str = "saya ganteng abiesss"
+
+for huruf in data_str:
+    print(huruf)
+print("akhir dari program 4\n")# jadi dia akan ngeprint per huruf nya sampai selesai secara brulang
+
