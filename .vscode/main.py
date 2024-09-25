99f6eddd0ee55046bf6877ccb77f1ea08b68f9bd
Difki Mahriza <Difky04zz@gmail.com>
2024-09-26 06:52:19 +0700

    nelajar python part26 (break)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,30 +1,29 @@
-#belajar python part25
-#continue pass
-
-#PASS fungsinya sbagai dummy (tidak akan dieksekusi)
-
-#cntoh
+#belajar python part26
+#break
+print("=====CONTOH FUNGSI BREAK KEREN=====")
+data_int = int(input("hitung sampai = "))
 angka = 0
-while angka < 5:
-    angka += 1
-    if angka == 3:
-        pass#ini tidak akan dieksekusi (hanya lewat aja)
-        # print("whatsup brow")
-    print(angka)
+while True:
+    angka +=1
+    print(f"count = {angka}")
+        
+    if angka == data_int:
+        print("niceee")
+        break#jadi yang 4 dan 5 nya gk di print dia langsung ke end (cukupppp)
 
-#CONTINUE
+        #NAH JADI DENGAN INI KITA BISA MENGEPRINT SBERAPA BNYAK MAU NYA
+    print("wassuppp bebeh!!!")
 
+print("cukupppp finishh!!")
+
+print("=====CONTOH SEDERHANA=====")
 angka = 0
-print(f"angka sekarang adalah {angka}")
-
-while angka < 5:
-    angka+=1
-    print(f"angka sekarang ialah {angka}")#ini aksi 1
-#lalu continue fungsiny gmn?
-#misal kita ingin buat kondisi saat dia sedang ngeloop, jadi cntoh saat selesai aksi 1 trus dikasih kondisi yng akan jadi pengecek, ketika dia blm memenuhi kondisi maka dia akan nge loop lagi
+while angka <5:
+    angka +=1
+    print(f"angka sekarang ialah {angka}")
+        
     if angka == 3:
-        print("niceee!!!")
-        continue #ini akan membuat  loop mlompat ke step slnjutny, jadi setiap ada di loop kalok ketemu continue dia akan balik lagi keatas, jadi intinya smua looping yang ada di bawah nya di skip dan loncat ke step slanjutnya
-    print("whassupp")#ini aksi 2
-print("finish")
-
+        print("niceee")
+        break #jadi dia langsung ke end gk lanjut ke 4 dan 5
+    print("wassuppp bebeh!!!")
+print("cukupp")
