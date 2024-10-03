de9117ad8f637742180690f50e0ed3e3d03a5374
Difki Mahriza <Difky04zz@gmail.com>
2024-10-03 08:31:31 +0700

    belajar python part 30 (operasi list)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,47 +1,33 @@
-#belajar python part29
-#manipulasi list
+#belajar python part30
+#operasi list
+data_angka = [1,2,3,2,3,9,5,1,7,9,9,2,3,4,5,6,5,4,2,4,5,6]
+print(f'data angka = \n{data_angka}')
 
-data = ['dudung','otong','sabar']#ini sebenernya ada index yang dimulai dari 0, dan kita juga bisa menggunakan index bernilai (-) jika kita mau mulai dari belakang, 0(-3), 1(-2), 2(-1)
+#1. Count Data
+#jadi kita bisa menghitung data ini muncul berapa kali di list data
+jumlah_data_4 = data_angka.count(4)
+jumlah_data_3= data_angka.count(3)
+print(f'jumlah angka 4 = {jumlah_data_4}')
+print(f'jumlah angka 3 = {jumlah_data_3}')
 
-#1. cara mengambil data dari list tersebut
-data_0=data[0]#0 sama value atau indexnya
-print(f"data pertama = {data_0}")
+#2.ambil posisi data
+data = ['ucup','otong','ujeng','parno','dudung']
+print(f'data = {data}')
+index_dudung=data.index('dudung')
+index_ujeng=data.index('ujeng')
+print(f'index si dudung ialah = {index_dudung}')
+print(f'index si ujeng ialah = {index_ujeng}')
 
-data_trakhir=data[-1]
-print(f"data terakhir = {data_trakhir}")
+#Mengurutkan list
+print(f"ini adalah data angka seblum di urut = {data}")
+data.sort()
+print(f'data setelah diurut = {data}')
+print(data_angka)
+data_angka.sort()
+print(f'data angka sort = {data_angka}')
 
-#mengambil info jumlah data
-panjang_data = len(data)
-print(f"panjang data = {panjang_data}") 
-
-#manipulasi data list
-
-#1.menambahkan item pada list sesuai posisi yang diinginkan
-print(f"data sebelum ditambah = \n{data}")
-data.insert(0,"ario")#nah pakai INSERT dimana (0 sebagai posisi yang ingin ditaruh, dan ario sebagai value yang ditambahkan)
-print(f'data setlah ditambhakan = \n{data}')
-
-#2.menambh di akhir list
-data.append("jajang")
-print(f'ini menambahkan data di akhir = {data}')
-
-#3. menambakan list dengan list
-data_baru = ['ujang','usep','dedeng']
-data.extend(data_baru)#jadi misal data baru mau ditambah ke data itu gunaakan EXTEND
-print(f'data gabungan = {data}')
-
-#4. merubah data
-#misal kita ingin ubah data ke dua , akan diganti dngn michael
-
-data[2]='michael'
-print(f'data rubah = {data}')#maka dari itu otong
-
-#meremove data
-data.remove('michael')
-print(f'ujang di remove = {data}')
-
-#meremove data paling blkang
-data.pop()#data pop ialah yang paling blkang
-print(f'data terakhir hilang = {data}')#nah skarang dedeng ilang, tapi misalkan kita taruh data.pop() kedalam variabel nnti yang ada ialah kita mengambil data terakhirny
-data_terakir=data.pop()
-print(data_terakir)#nah karena yang terakhir kali di run itu urutan nya usep, maka pas di print usep ikutin 
+#membalik list nya
+data.reverse()
+data_angka.reverse()
+print(data)
+print(data_angka)
