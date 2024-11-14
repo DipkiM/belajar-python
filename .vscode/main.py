f99bbba709e86970e0c309bb6f756ee88fa8721c
Difki Mahriza <Difky04zz@gmail.com>
2024-11-14 08:51:59 +0700

    belajar python part 31 (copy list)

--- .vscode/main.py
+++ .vscode/main.py
@@ -1,33 +1,41 @@
-#belajar python part30
-#operasi list
-data_angka = [1,2,3,2,3,9,5,1,7,9,9,2,3,4,5,6,5,4,2,4,5,6]
-print(f'data angka = \n{data_angka}')
+#belajar python part31
+#copy list
+#teknik menduplikat list
 
-#1. Count Data
-#jadi kita bisa menghitung data ini muncul berapa kali di list data
-jumlah_data_4 = data_angka.count(4)
-jumlah_data_3= data_angka.count(3)
-print(f'jumlah angka 4 = {jumlah_data_4}')
-print(f'jumlah angka 3 = {jumlah_data_3}')
+#ini adalah teknik pass by reference (dengan address yang sama)
+a = ['ucup','otong','dudung']
+print(f"a={a}")
+b=a
+print(f"b={b}")
 
-#2.ambil posisi data
-data = ['ucup','otong','ujeng','parno','dudung']
-print(f'data = {data}')
-index_dudung=data.index('dudung')
-index_ujeng=data.index('ujeng')
-print(f'index si dudung ialah = {index_dudung}')
-print(f'index si ujeng ialah = {index_ujeng}')
+#kita akan merubah member dari a
+a[1]="michael"
+#maka akan merubah b juga pada indeks 1
 
-#Mengurutkan list
-print(f"ini adalah data angka seblum di urut = {data}")
-data.sort()
-print(f'data setelah diurut = {data}')
-print(data_angka)
-data_angka.sort()
-print(f'data angka sort = {data_angka}')
+b.sort()
+#bahkan saat kita merubah b a juga ikut berubah
 
-#membalik list nya
-data.reverse()
-data_angka.reverse()
-print(data)
-print(data_angka)
+print(f"a={a}")
+print(f"b={b}")
+
+#address dari kedua list a dan b
+print(f"address a={hex(id(a))}")
+print(f"address b={hex(id(b))}")
+#nah ini kita liat bahwasanya address a dan b sama, ini menjelaskan bahwa mereka itu sama. pengaruhnya apa?
+
+#itu tadi bukan mengcopy karena dia berhubungan satu sama lain
+
+#mari kita menduplikat dengan cara mengcopy
+print("membuat list c, dengan a.copy")
+c= a.copy() #ini cara dengan copy atau data baru address baru (full duplikat)
+print(c)
+print(f"address a={hex(id(a))}")
+print(f"address b={hex(id(b))}")
+print(f"address c={hex(id(c))}")
+
+#nah sekarang kita punya address c yang beda, nah itu berarti kita membuat c list baru dengan address yang beda, shingga kita dapat data yang sama dengan si a, tapi ketika kita ingin merubah si c, si a dan b tidak akan berubah, karena address nya beda
+
+c[1]= "suneo"
+print(c)
+print(b)
+print(a)
